2eae8ac9500dddcb32dde2d80fbf59d8d318b4c2
ghasem shirdel <shirdelghasem79@gmail.com>
2020-09-02 21:09:08 +0430

    add navigation component and main fragment with toolbar

---
+++ app/src/main/java/com/jakode/contacts/ui/fragments/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.jakode.contacts.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jakode.contacts.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabButton.setOnClickListener {
+            // Safe args
+            val action = MainFragmentDirections.actionMainFragmentToAddUserFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
