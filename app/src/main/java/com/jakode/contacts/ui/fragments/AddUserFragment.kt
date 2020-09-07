ad139d3abd283226c4627af34043d9f55578c445
ghasem shirdel <shirdelghasem79@gmail.com>
2020-09-07 18:06:32 +0430

    fix bug

--- app/src/main/java/com/jakode/contacts/ui/fragments/AddUserFragment.kt
+++ app/src/main/java/com/jakode/contacts/ui/fragments/AddUserFragment.kt
@@ -1,12 +1,10 @@
 package com.jakode.contacts.ui.fragments
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -56,7 +54,6 @@ class AddUserFragment : Fragment() {
         emailOnClick()
 
         binding.cancel.setOnClickListener {
-            hideKeyboard(requireContext(), it)
             findNavController().navigateUp()
         }
     }
@@ -114,11 +111,6 @@ class AddUserFragment : Fragment() {
                 Toast.LENGTH_SHORT
             ).show()
         }
-    }
-
-    private fun hideKeyboard(context: Context, view: View) {
-        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onDestroyView() {
